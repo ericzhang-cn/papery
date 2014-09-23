f06b4ee16c60962e2c0a3d20eff0cbb36dc36eaa
ericzhang <ericzhang.buaa@gmail.com>
2014-09-23 11:39:54 +0800

    fix bugs

--- bin/papery.js
+++ bin/papery.js
@@ -38,35 +38,32 @@ var checkArgs = function (args) {
     if (args.length >= 2) {
         if (args[1] === '-v' || args[1] === '--version') {
             version();
-            return false;
+            process.exit(1);
         }
         if (args[1] === '-h' || args[1] === '--help') {
             help();
-            return false;
+            process.exit(1);
         }
         if (args.length >= 3 && args[1] === 'help' && _.contains(cmds, args[2])) {
             showHelp(args[2]);
-            return false;
+            process.exit(1);
         }
         if (!_.contains(cmds, args[1])) {
-            return false;
+            help();
+            process.exit(1);
         }
-        return true;
+        return 0;
     } else {
-        return false;
+        help();
+        process.exit(1);
     }
-
 };
 
 var args = !(process.argv[0] === 'node' ||
     process.argv[0] === 'nodejs' ||
     process.argv[0].match(/node.exe$/)) ? process.argv : _.last(process.argv, process.argv.length - 1);
 
-if (checkArgs(args) === false) {
-    help();
-    process.exit(1);
-}
-
+checkArgs(args);
 var cmd = args[1];
 var options = _.last(args, args.length - 2);
 
